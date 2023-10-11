a16d8fa67604a97c9e3fd5344a99947eb4d4b684
dragonjump <116328950+dragonjump@users.noreply.github.com>
2023-10-11 19:55:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker installed');
-  });
-  
-  self.addEventListener('activate', event => {
-    console.log('Service Worker activated');
-  });
-  
-  self.addEventListener('fetch', event => {
-    console.log('Fetch event:', event.request.url);
-  });
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch event:", event.request.url);
+});
+
+// Listen to "push" events in the service worker.
+self.addEventListener("push", (event) => {
+  alert("intercepted 1");
+  // Set or clear the badge.
+  if (navigator.setAppBadge) {
+    navigator.setAppBadge(55);
+    alert("intercepted 2");
+  }
+});
