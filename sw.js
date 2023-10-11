4c74c72aa288062a56f642c804634d576f4822fc
dragonjump <116328950+dragonjump@users.noreply.github.com>
2023-10-11 20:02:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,3 +19,25 @@ self.addEventListener("push", (event) => {
     alert("intercepted 2");
   }
 });
+
+self.addEventListener('notificationclick', function (event)
+{
+    //For root applications: just change "'./'" to "'/'"
+    //Very important having the last forward slash on "new URL('./', location)..."
+    const rootUrl = new URL('./', location).href; 
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll().then(matchedClients =>
+        {
+            for (let client of matchedClients)
+            {
+                if (client.url.indexOf(rootUrl) >= 0)
+                {
+                    return client.focus();
+                }
+            }
+
+            return clients.openWindow(rootUrl).then(function (client) { client.focus(); });
+        })
+    );
+});
