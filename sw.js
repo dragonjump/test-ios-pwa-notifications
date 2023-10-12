1fe4365bb76fc38876023efa9a12418b72fbef4a
dragonjump <116328950+dragonjump@users.noreply.github.com>
2023-10-12 08:29:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -25,22 +25,22 @@ self.addEventListener('notificationclick', function (event)
     //For root applications: just change "'./'" to "'/'"
     //Very important having the last forward slash on "new URL('./', location)..."
     const rootUrl = new URL('./', location).href; 
+  
+    // event.waitUntil(
+    //     clients.matchAll().then(matchedClients =>
+    //     {
+    //         for (let client of matchedClients)
+    //         {
+    //             if (client.url.indexOf(rootUrl) >= 0)
+    //             {
+    //                 return client.focus();
+    //             }
+    //         }
+    //         console.log('yes')
+    //         return clients.openWindow(rootUrl+'/aa').then(function (client) { client.focus(); });
+    //     })
+    // );
     event.notification.close();
-    event.waitUntil(
-        clients.matchAll().then(matchedClients =>
-        {
-            for (let client of matchedClients)
-            {
-                if (client.url.indexOf(rootUrl) >= 0)
-                {
-                    return client.focus();
-                }
-            }
-            console.log('yes')
-            return clients.openWindow(rootUrl+'/aa').then(function (client) { client.focus(); });
-        })
-    );
-
     console.log('yes')
     clients.openWindow('https://google.com');
 });
